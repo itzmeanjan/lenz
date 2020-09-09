037dd241041dadad4bd86f59121ea1e518eb5058
Anjan Roy <anjanroy@yandex.com>
2020-09-09 20:43:30 +0530

    fixed response json structure, added indentation

--- server/index.js
+++ server/index.js
@@ -9,18 +9,18 @@ const app = express()
 
 app.get('/ip/:addr', (req, res) => {
     if (isIP(req.params.addr) === 0) {
-        return res.status(400).json({ msg: 'Bad Input' })
+        return res.status(400).contentType('application/json').send(JSON.stringify({
+            msg: 'Bad Input'
+        }, space = '\t'))
     }
 
-    console.log(ip2location.IP2Location_get_all(req.params.addr))
-
-    return res.status(200).json({
+    return res.status(200).contentType('application/json').send(JSON.stringify({
         ip: req.params.addr,
         lon: ip2location.IP2Location_get_longitude(req.params.addr),
         lat: ip2location.IP2Location_get_latitude(req.params.addr),
         region: ip2location.IP2Location_get_region(req.params.addr),
         country: ip2location.IP2Location_get_country_long(req.params.addr)
-    })
+    }, space = '\t'))
 })
 
 http.createServer(app).listen(8000, '0.0.0.0',
