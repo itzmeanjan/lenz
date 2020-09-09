3b7fab54953ba9284366700c095b2e74fc797c3a
Anjan Roy <anjanroy@yandex.com>
2020-09-09 20:38:21 +0530

    added ip2location lib, querying location (lon & lat), country info of ip

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const http = require('http')
 const isIP = require('net').isIP
+const ip2location = require('ip2location-nodejs')
+
+ip2location.IP2Location_init('../IP2LOCATION-LITE-DB5.IPV6.BIN')
 
 const app = express()
 
@@ -9,7 +12,15 @@ app.get('/ip/:addr', (req, res) => {
         return res.status(400).json({ msg: 'Bad Input' })
     }
 
-    return res.status(200).json({ addr: req.params.addr })
+    console.log(ip2location.IP2Location_get_all(req.params.addr))
+
+    return res.status(200).json({
+        ip: req.params.addr,
+        lon: ip2location.IP2Location_get_longitude(req.params.addr),
+        lat: ip2location.IP2Location_get_latitude(req.params.addr),
+        region: ip2location.IP2Location_get_region(req.params.addr),
+        country: ip2location.IP2Location_get_country_long(req.params.addr)
+    })
 })
 
 http.createServer(app).listen(8000, '0.0.0.0',
