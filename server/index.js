52a41f0bc8fcf71f5626d91bee34a442ee0f4fdd
Anjan Roy <anjanroy@yandex.com>
2020-09-09 20:11:06 +0530

    validating ip address from input

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const http = require('http')
+const isIP = require('net').isIP
 
 const app = express()
 
-app.get('/lookup/:addr', (req, res) => {
-    res.status(200).send(req.params.addr)
+app.get('/ip/:addr', (req, res) => {
+    if (isIP(req.params.addr) === 0) {
+        return res.status(404).json({ msg: 'Bad Input' })
+    }
+
+    return res.status(200).json({ addr: req.params.addr })
 })
 
 http.createServer(app).listen(8000, '0.0.0.0',
