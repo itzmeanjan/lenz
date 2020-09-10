9a68d4f34d1669960187847fea63aedfa63d8e4a
Anjan Roy <anjanroy@yandex.com>
2020-09-10 23:29:41 +0530

    added code comment

--- magneto/ip.js
+++ magneto/ip.js
@@ -1,6 +1,8 @@
 const axios = require('axios')
 const endpoint = 'https://api.ipify.org?format=json'
 
+// Look up my ip address by sending HTTP GET request to
+// third party endpoint
 const getMyIP = _ => new Promise((resolve, reject) => {
     axios.get(endpoint).then(resp => resolve(resp.data.ip)).catch(reject)
 })
--- magneto/locate.js
+++ magneto/locate.js
@@ -2,10 +2,13 @@ const dotnet = require('dotenv')
 dotnet.config({ path: 'config.env' })
 
 const ip2location = require('ip2location-nodejs')
-ip2location.IP2Location_init(process.env.DB)
+ip2location.IP2Location_init(process.env.DB) // initializing database
 
 const { isIP } = require('net')
 
+// Given a valid IPv{4, 6} address, it'll lookup
+// its location i.e. country, region, longitude & latitude
+// and return as an object
 const lookup = ip => isIP(ip) === 0 ? null : {
     ip: ip,
     lon: ip2location.IP2Location_get_longitude(ip),
