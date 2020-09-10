b13de3346402218a591a4271edc6a1eaef5d9ff4
Anjan Roy <anjanroy@yandex.com>
2020-09-11 00:10:31 +0530

    coloring terminal; checking input magnet link

--- magneto/index.js
+++ magneto/index.js
@@ -3,6 +3,8 @@
 const yargs = require('yargs')
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
+const magnet = require('magnet-uri')
+colors = require('colors')
 
 const { getMyIP } = require('./ip')
 const { lookup } = require('./locate')
@@ -11,9 +13,14 @@ const validateLookup = data => !(data.lon === 0 && data.lat === 0 && data.region
 
 const options = yargs
     .usage("Usage: -m <magnet>")
-    .option("m", { alias: "magnet", describe: "Torrent Magnet 🧲 Link", type: "string", demandOption: true })
+    .option("m", { alias: "magnet", describe: "Torrent 🧲 Link", type: "string", demandOption: true })
     .argv;
 
+const parsed = magnet(options.m)
+if(!parsed.infoHash) {
+    console.log('[!]Bad Torrent 🧲 Link'.red)
+    process.exit(1)
+}
 
 const screen = blessed.screen()
 const map = contrib.map({ label: 'World Map', style: { shapeColor: 'cyan' } })
