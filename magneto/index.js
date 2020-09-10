5cd8598f4620d14ffc6853164a1a35cefcb0dd86
Anjan Roy <anjanroy@yandex.com>
2020-09-10 12:21:05 +0530

    basic map with proper bounds set

--- magneto/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', _ => {
-    const map = L.map('map').setView([51.505, -0.09], 1)
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-})
