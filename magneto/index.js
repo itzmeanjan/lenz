ae8773153eb391ad65d0ea9a9cc7ada31537e5ac
Anjan Roy <anjanroy@yandex.com>
2020-09-10 23:11:15 +0530

    show ip address of host machine in map

--- magneto/index.js
+++ magneto/index.js
@@ -1,12 +1,21 @@
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
 
+const {getMyIP} = require('./ip')
+const {lookup} = require('./locate')
+
 const screen = blessed.screen()
 const map = contrib.map({ label: 'World Map', style: { shapeColor: 'cyan' } })
 
 screen.append(map)
-map.addMarker({ "lon": "0", "lat": "0", color: "red", char: "*" })
+
+getMyIP().then(ip => {
+    let resp  = lookup(ip)
+    if(!resp) {
+        map.addMarker({ "lon": resp.lon, "lat": resp.lat, color: "red", char: "X" })        
+    }
+
+    screen.render()
+})
 
 screen.key(['escape', 'q', 'C-c'], (ch, key) => process.exit(0))
-
-screen.render()
