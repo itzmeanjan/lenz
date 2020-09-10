b35f5e4a0eb3c6776ea333a3006181231be98c87
Anjan Roy <anjanroy@yandex.com>
2020-09-11 00:17:58 +0530

    added code comment

--- magneto/index.js
+++ magneto/index.js
@@ -9,13 +9,18 @@ colors = require('colors')
 const { getMyIP } = require('./ip')
 const { lookup } = require('./locate')
 
+// validates looked up ip address info, because in case of
+// private ip addresses it'll return longitude & latitude fields as `0` & region & country as `-`
 const validateLookup = data => !(data.lon === 0 && data.lat === 0 && data.region === '-' && data.country === '-')
 
+// taking valid magnet link as input from user
 const options = yargs
     .usage("Usage: -m <magnet>")
     .option("m", { alias: "magnet", describe: "Torrent 🧲 Link", type: "string", demandOption: true })
     .argv;
 
+// if gets parsed properly & `infoHash` of torrent can
+// be found, then we're good to go, otherwise, we'll exit
 const parsed = magnet(options.m)
 if(!parsed.infoHash) {
     console.log('[!]Bad Torrent 🧲 Link'.red)
@@ -27,13 +32,17 @@ const map = contrib.map({ label: 'World Map', style: { shapeColor: 'cyan' } })
 
 screen.append(map)
 
+// obtaining ip address of this machine, by sending query to
+// 'https://api.ipify.org?format=json'
 getMyIP().then(ip => {
     let resp = lookup(ip)
     if (validateLookup(resp)) {
+        // once location info available, adding this machine's location onto map
         map.addMarker({ lon: resp.lon, lat: resp.lat, color: "red", char: "X" })
         screen.render()
     }
 })
 
+// pressing {esc, q, ctrl+c}, results into exit with success i.e. return value 0
 screen.key(['escape', 'q', 'C-c'], (ch, key) => process.exit(0))
 screen.render()
