5afb11e91cba8063fdc64667d27e87e7ce6bab9f
Anjan Roy <anjanroy@yandex.com>
2020-09-10 21:33:18 +0530

    added style to map

--- magneto/index.js
+++ magneto/index.js
@@ -2,13 +2,11 @@ const blessed = require('blessed')
 const contrib = require('blessed-contrib')
 
 const screen = blessed.screen()
-const map = contrib.map({ label: 'World Map' })
+const map = contrib.map({ label: 'World Map', style: { shapeColor: 'cyan' } })
 
 screen.append(map)
-map.addMarker({ "lon": "-79.0000", "lat": "37.5000", color: "red", char: "*" })
+map.addMarker({ "lon": "0", "lat": "0", color: "red", char: "*" })
 
-screen.key(['escape', 'q', 'C-c'], function (ch, key) {
-    return process.exit(0);
-});
+screen.key(['escape', 'q', 'C-c'], (ch, key) => process.exit(0))
 
 screen.render()
