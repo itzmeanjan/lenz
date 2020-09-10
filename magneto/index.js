68a51873a396dc39e3937f346953e697ccd2eede
Anjan Roy <anjanroy@yandex.com>
2020-09-10 12:44:11 +0530

    added browserify, for bundling dependencies for web

--- magneto/index.js
+++ magneto/index.js
@@ -1,3 +1,6 @@
+const DHT = require('bittorrent-dht')
+const magnetParser = require('magnet-uri')
+
 const map = L.map('map', {
     zoomControl: false,
     minZoom: 2,
@@ -6,7 +9,12 @@ const map = L.map('map', {
     maxBounds: [[90, -180], [-90, 180]]
 }).setView([0, 0], 2)
 
-map.whenReady( _ => { console.log('ready') })
+map.whenReady(_ => {
+    const uri = 'magnet:?xt=urn:btih:e3811b9539cacff680e418124272177c47477157'
+    const parsed = magnetParser(uri)
+
+    console.log(parsed.infoHash)
+})
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     {
