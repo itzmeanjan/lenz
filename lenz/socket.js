acdf4c5da389437b7c74da5fa6b916abe064c313
Anjan Roy <anjanroy@yandex.com>
2020-09-14 19:47:10 +0530

    written different fucntion for running `lsof` command

--- lenz/socket.js
+++ lenz/socket.js
@@ -1,8 +1,27 @@
 const { spawn } = require('child_process')
 
-const getTCPAndUDPPeers = _ => new Promise((resolve, reject) => {
+// running lsof -i command on console, 
+// on a different process in async mode
+//
+// returning string representation of data
+const lsof = _ => new Promise((resolve, reject) => {
+    let buffer = null
     const lsof = spawn('lsof', ['-i'])
+
+    lsof.stdout.on('data', d => { buffer = d })
+
+    lsof.on('close', code => {
+        if (code !== 0) {
+            reject(code)
+        }
+
+        resolve(buffer.toString())
+    })
+})
+
+const getTCPAndUDPPeers = _ => new Promise((resolve, reject) => {
     const buffer = []
+    const lsof = spawn('lsof', ['-i'])
 
     lsof.stdout.on('data', d => {
         buffer.push(d)
@@ -17,3 +36,6 @@ const getTCPAndUDPPeers = _ => new Promise((resolve, reject) => {
     })
 })
 
+module.exports = {
+    getTCPAndUDPPeers
+}
