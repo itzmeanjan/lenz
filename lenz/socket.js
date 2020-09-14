2ad84ff549f8e03f4744510b6b7cb6dc1b62aaac
Anjan Roy <anjanroy@yandex.com>
2020-09-14 21:13:29 +0530

    pipelining of instructions in same function

--- lenz/socket.js
+++ lenz/socket.js
@@ -1,44 +1,29 @@
 const { spawn } = require('child_process')
 
-// running lsof -i command on console, 
-// on a different process in async mode
-//
-// returning string representation of data
-const lsof = _ => new Promise((resolve, reject) => {
+const getTCPAndUDPPeers = _ => new Promise((resolve, reject) => {
     let buffer
     const lsof = spawn('lsof', ['-i'])
+    const awk = spawn('awk', ['{ print $9 }'])
 
-    lsof.stdout.on('data', d => { buffer = d })
+    lsof.stdout.on('data', d => {
+        awk.stdin.write(d.toString())
+    })
     lsof.on('close', code => {
         if (code !== 0) {
             reject(code)
         }
-
-        resolve(buffer.toString())
+        awk.stdin.end()
     })
-})
 
-// running awk command with out put from `lsof` command
-// in which some pattern matching to be performed
-// for extracting useful information from certain column ( here 9-th )
-const awk = data => new Promise((resolve, reject) => {
-    let buffer
-    const awk = spawn('awk', ["'{ print $9 }'"])
-
-    awk.stdout.on('data', d => { buffer = d })
+    awk.stdout.on('data', d => {
+        buffer = d.toString()
+    })
     awk.on('close', code => {
         if (code !== 0) {
             reject(code)
         }
-
-        resolve(buffer.toString())
+        resolve(buffer)
     })
-
-    awk.stdin.write(data)
-})
-
-const getTCPAndUDPPeers = _ => new Promise((resolve, reject) => {
-    lsof().then(v => { awk(v).then(resolve).catch(reject) }).catch(reject)
 })
 
 module.exports = {
