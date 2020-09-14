53346d5fd5dc18a30be8eae630c61b0f13da833c
Anjan Roy <anjanroy@yandex.com>
2020-09-14 19:51:44 +0530

    primary awk processor written

--- lenz/socket.js
+++ lenz/socket.js
@@ -5,13 +5,29 @@ const { spawn } = require('child_process')
 //
 // returning string representation of data
 const lsof = _ => new Promise((resolve, reject) => {
-    let buffer = null
+    let buffer
     const lsof = spawn('lsof', ['-i'])
 
     lsof.stdout.on('data', d => { buffer = d })
-
     lsof.on('close', code => {
         if (code !== 0) {
+            reject(code)
+        }
+
+        resolve(buffer.toString())
+    })
+})
+
+// running awk command with out put from `lsof` command
+// in which some pattern matching to be done
+// for extracting useful information column
+const awk = _ => new Promise((resolve, reject) => {
+    let buffer
+    const awk = spawn('awk', ["'{ print $9 }'"])
+
+    awk.stdout.on('data', d => { buffer = d })
+    awk.on('close', code => {
+        if(code !== 0) {
             reject(code)
         }
 
