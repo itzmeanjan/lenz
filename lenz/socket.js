0f81d89d18e18e48bfadfbdf46f7387fe0af82e1
Anjan Roy <anjanroy@yandex.com>
2020-09-14 19:55:19 +0530

    completed pipeline setup

--- lenz/socket.js
+++ lenz/socket.js
@@ -19,37 +19,26 @@ const lsof = _ => new Promise((resolve, reject) => {
 })
 
 // running awk command with out put from `lsof` command
-// in which some pattern matching to be done
-// for extracting useful information column
-const awk = _ => new Promise((resolve, reject) => {
+// in which some pattern matching to be performed
+// for extracting useful information from certain column ( here 9-th )
+const awk = data => new Promise((resolve, reject) => {
     let buffer
     const awk = spawn('awk', ["'{ print $9 }'"])
 
     awk.stdout.on('data', d => { buffer = d })
     awk.on('close', code => {
-        if(code !== 0) {
+        if (code !== 0) {
             reject(code)
         }
 
         resolve(buffer.toString())
     })
+
+    awk.stdin.write(data)
 })
 
 const getTCPAndUDPPeers = _ => new Promise((resolve, reject) => {
-    const buffer = []
-    const lsof = spawn('lsof', ['-i'])
-
-    lsof.stdout.on('data', d => {
-        buffer.push(d)
-    })
-
-    lsof.on('close', code => {
-        if (code !== 0) {
-            reject(code)
-        }
-
-        resolve(buffer)
-    })
+    lsof().then(v => { awk(v).then(resolve).catch(reject) }).catch(reject)
 })
 
 module.exports = {
