00423c73fb83b991fbca8aa032e0df226c9c5fda
Anjan Roy <anjanroy@yandex.com>
2020-09-16 20:56:04 +0530

    extract out elements from html page

--- lenz/resources.js
+++ lenz/resources.js
@@ -1,9 +1,21 @@
 const axios = require('axios')
+const cheerio = require('cheerio')
 
+// given url, fetches html content of that page
 const getHTML = url => new Promise((resolve, reject) => {
     axios.default.get(url).then(resp => resolve(resp.data)).catch(reject)
 })
 
+// extract out specified html element(s) from html page
+const extractElementsFromHTML = (html, element) => {
+    const $ = cheerio.load(html)
+
+    $(element).each((i, v) => {
+        console.log(v)
+    })
+}
+
 module.exports = {
-    getHTML
+    getHTML,
+    extractElementsFromHTML
 }
