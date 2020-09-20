54ec3381719a8a7c2bbfaaf5fa0b2a02bb0eb864
Anjan Roy <anjanroy@yandex.com>
2020-09-20 15:03:48 +0530

    now also lookup geolocation info of generated ip from range

--- lenz/range.js
+++ lenz/range.js
@@ -3,15 +3,16 @@ const { Address4 } = require('ip-address')
 class IPv4Range {
 
     // ipv4 address range, where from & two both are integers
-    constructor(from, to) {
+    constructor(from, to, glass) {
         this.from = from
         this.to = to
+        this.glass = glass
     }
 
     // generate all ipv4 addresses as lazy stream, within this range
     *all() {
         for (let cur = this.from; cur <= this.to; cur++) {
-            yield Address4.fromInteger(cur).address
+            yield this.glass(Address4.fromInteger(cur).address)
         }
     }
 
