ffae8d05e947801e18e484af45dd5a0588e79509
Anjan Roy <anjanroy@yandex.com>
2020-09-20 14:39:36 +0530

    converting numeric ip address to period seperated form

--- lenz/range.js
+++ lenz/range.js
@@ -1,3 +1,5 @@
+const { Address4 } = require('ip-address')
+
 class IPv4Range {
 
     // ipv4 address range, where from & two both are integers
@@ -9,7 +11,7 @@ class IPv4Range {
     // generate all ipv4 addresses as lazy stream, within this range
     all() {
         for (let cur = this.from; cur <= this.to; cur++) {
-            yield cur
+            yield Address4.fromInteger(i).address
         }
     }
 
