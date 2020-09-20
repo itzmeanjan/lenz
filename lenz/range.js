32352271fd8e38221dd9103fd8d589ec6957d4eb
Anjan Roy <anjanroy@yandex.com>
2020-09-20 15:19:53 +0530

    added code comment

--- lenz/range.js
+++ lenz/range.js
@@ -3,6 +3,10 @@ const { Address4 } = require('ip-address')
 class IPv4Range {
 
     // ipv4 address range, where from & two both are integers
+    // glass is like looking glass, for finding geo location data
+    // of ip address, using ip2location db5
+    // so glass is nothing but a function, when an ip address is passed
+    // it can return location info
     constructor(from, to, glass) {
         this.from = from
         this.to = to
