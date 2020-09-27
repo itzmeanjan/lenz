4241b0c3a682fb7af1a13bd21fd69906ccb0df97
Anjan Roy <anjanroy@yandex.com>
2020-09-27 11:33:59 +0530

    due to implementation of seperation of concern, only functions are now imported from other file

--- lenz/index.js
+++ lenz/index.js
@@ -2,8 +2,7 @@
 
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
-const magnet = require('magnet-uri')
-const { existsSync, writeFile } = require('fs')
+const { writeFile } = require('fs')
 const { isIP } = require('net')
 const dns = require('dns')
 const isValidDomain = require('is-valid-domain')
@@ -17,47 +16,8 @@ const { getMyIP } = require('./ip')
 const { init, lookup } = require('./locate')
 const { getHTML, extractCSSResources, extractScriptResources, extractImageResources, extractDomainNamesFromURLs, mergetTwoSets } = require('./resources')
 const { Worker } = require('worker_threads')
+const { checkMagnetLinkValidation, checkDBExistance, checkDomainNameValidation, validateLookup, checkIPAddressValidation } = require('./util')
 
-// validating user given torrent magnet link
-const checkMagnetLinkValidation = _magnet => {
-    const parsed = magnet(_magnet)
-    if (!parsed.infoHash) {
-        console.log('[!]Bad Torrent 🧲 Link'.red)
-        process.exit(1)
-    }
-
-    return parsed.infoHash
-}
-
-// checking existance of local ip2location db file {*.bin, *.csv}
-// if not present, exit process with return code 1
-const checkDBExistance = db => {
-    if (!existsSync(db)) {
-        console.log('[!]Can\'t find IP2Location Database'.red)
-        process.exit(1)
-    }
-}
-
-// checking whether supplied domain name is valid or not
-// if not, exit process with return code 1
-const checkDomainNameValidation = domain => {
-    if (!isValidDomain(domain)) {
-        console.log('[!]Invalid domain name'.red)
-        process.exit(1)
-    }
-}
-
-// validates looked up ip address info, because in case of
-// private ip addresses it'll return longitude & latitude fields as `0` & region & country as `-`
-const validateLookup = data => !(data.lon === 0 && data.lat === 0 && data.region === '-' && data.country === '-')
-
-// validates user provided IPv{4,6} address
-const checkIPAddressValidation = ip => {
-    if (!isIP(ip)) {
-        console.log('[!]Invalid IP Address'.red)
-        process.exit(1)
-    }
-}
 
 // add marker on map in specified location
 const addMarkerAndRender = (lon, lat, color, char, map, screen) => {
